9536ba2d9a40731fb12858f7ac7214c0306b0388
gehaipeng <ghp628@gmail.com>
2015-11-25 09:19:30 +0800

    base request class

--- ZGFrameWorks/NetWorking/ZGRequest.h
+++ ZGFrameWorks/NetWorking/ZGRequest.h
@@ -9,5 +9,29 @@
 #import <Foundation/Foundation.h>
 #import "ZGBaseRequest.h"
 @interface ZGRequest : ZGBaseRequest
+@property (nonatomic) BOOL ignoreCache;
 
+// 返回当前缓存的对象
+- (id) cacheJson;
+
+
+// 是否当前的数据从缓存获得
+- (BOOL)isDataFromCache;
+
+//返回当前缓存是否需要更新
+- (BOOL)isCacheVersionExpired;
+
+//强制更新缓存
+- (void)startWithoutCache;
+
+//手动将其他请求的respone 写入该请求
+-(void)saveJsonResponeToCacheFile:(id)json;
+
+
+// for subclass to overwrite
+- (NSInteger)cacheTimeInSeconds;
+
+- (long long)cacheVersion;
+
+- (id)cacheSensitiveData;
 @end
--- ZGFrameWorks/NetWorking/ZGRequest.m
+++ ZGFrameWorks/NetWorking/ZGRequest.m
@@ -7,7 +7,62 @@
 //
 
 #import "ZGRequest.h"
+#import "ZGNetworkConfig.h"
+#import "ZGNetworkPrivate.h"
+
+@interface ZGRequest ()
+@property (strong, nonatomic) id cacheJson;
+
+@end
 
 @implementation ZGRequest
+
+- (NSInteger) cacheTimeInSeconds{
+
+    return -1;
+}
+- (long long)cacheVersion{
+
+    return 0;
+}
+
+- (id)cacheSensitiveData{
+    return nil;
+}
+
+- (void)checkDirectory:(NSString *)path{
+    BOOL isDir;
+    NSFileManager *fileManager=[NSFileManager defaultManager];
+    if (![fileManager fileExistsAtPath:path isDirectory:&isDir]) {
+        
+    }else
+    {
+        if (!isDir) {
+            NSError *error =nil;
+            [fileManager removeItemAtPath:path error:&error];
+        }
+    
+    }
+
+}
+
+- (void)createBaseDirectoryAtPath:(NSString *)path{
+    __autoreleasing NSError *error =nil;
+    [[NSFileManager defaultManager] createDirectoryAtPath:path
+                             withIntermediateDirectories :YES
+                                              attributes :nil
+                                                   error :&error];
+    if (error) {
+        ZGLog(@"create cache directory failed, error=%@",error);
+    }else{
+        
+    }
+        
+
+
+
+
+}
+
 
 @end
